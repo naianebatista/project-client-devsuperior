ecb3c08f718970e83f2dfabb24277045729b3990
naianebatista <nbatista@ciandt.com>
2022-10-07 15:33:38 -0300

    Project Created

---
+++ projectclient/src/main/java/com/devsuperior/projectclient/projectclient/ProjectclientApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.projectclient.projectclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectclientApplication.class, args);
+	}
+
+}
---
+++ projectclient/src/test/java/com/devsuperior/projectclient/projectclient/ProjectclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.projectclient.projectclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
